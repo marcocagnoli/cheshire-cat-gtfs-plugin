3498b1b5d199b4c5b50726dbb00dd2cc6409d151
marco-cagnoli <marco.cagnoli@romamobilita.it>
2023-04-18 12:16:51 +0200

    First commit

---
+++ gtfs/gtfs.py
@@ -0,0 +1,98 @@
+from cat.mad_hatter.decorators import tool, hook
+import requests
+import json
+from prettytable import PrettyTable
+from datetime import datetime, timezone
+from dateutil import tz
+import pytz
+
+@tool(return_direct=True)
+def bus_stop_eta (stop_code: str, cat):
+    """Use to retrieve what bus is arriving at a stop. Input is the code (or the number) of the bus stop and should be wrapped into double quotes."""
+
+    # ----- start configuration ---------
+    
+    # get a free api key from https://www.transit.land/
+    
+    transitland_apikey = "<apikey>"
+    
+    # in the same websie, find your feed onestopid. For example, for Rome, it is "f-sr-atac~romatpl~trenitalia"
+    
+    onestopid = "<onestopid>"
+    
+    # ----- end  configuration ---------
+    
+    url = "https://transit.land/api/v2/rest/stops/" + onestopid + ":" + stop_code + "/departures"
+    headers = {"apikey": transitland_apikey}
+    
+    response = requests.get(url, headers=headers)
+    
+    eta = response.json()
+    
+    from_zone = tz.gettz('UTC')
+    to_zone = tz.gettz('Europe/Rome')
+
+    for stop in eta['stops']:
+        
+        stop_name = stop["stop_name"]
+        
+        answer = "These are next arrivals at bus stop \"" + stop_name + "\" (" + stop_code + "):\n\n"
+    
+        routes = []
+        
+        for departure in stop['departures']:
+    
+            realtime = 'NO'
+            
+            if departure['arrival']['estimated'] is not None:
+                realtime = 'YES'
+            
+            if realtime == "YES":
+                
+                if departure['trip']['route']['route_short_name'] not in routes:
+                    routes.append(departure['trip']['route']['route_short_name'])
+                
+                if len(routes) == 1:
+                    answer += "REAL TIME DATA" + "\n\n"
+                
+                answer += "Route: " + departure['trip']['route']['route_short_name'] + "\n"
+                answer += "Company: " + departure['trip']['route']['agency']['agency_name'] +"\n"
+                answer += "Destination: " + departure['trip']['trip_headsign'] + "\n"
+                
+                answer += "Arriving at: " + departure['arrival']['estimated'] + "\n"
+
+                answer += "Minutes: " + str(round(((datetime.strptime(departure['arrival']['estimated'], "%H:%M:%S") - datetime.strptime(convertUTCToRomeTime(datetime.now()).strftime("%H:%M:%S"), "%H:%M:%S")).total_seconds()) / 60)) + "\n\n"
+                
+        l = len(routes)
+        
+        for departure in stop['departures']:
+    
+            realtime = 'NO'
+            
+            if departure['arrival']['estimated'] is not None:
+                realtime = 'YES'
+            
+            if realtime == "NO":
+                
+                if departure['trip']['route']['route_short_name'] not in routes:
+                    routes.append(departure['trip']['route']['route_short_name'])
+                
+                    if (len(routes) -l) == 1:
+                        answer += "SCHEDULED DATA" + "\n\n"
+                
+                    answer += "Route: " + departure['trip']['route']['route_short_name'] + "\n"
+                    answer += "Company: " + departure['trip']['route']['agency']['agency_name'] +"\n"
+                    answer += "Destination: " + departure['trip']['trip_headsign'] + "\n"
+                    
+                    answer += "Arriving at (SCHEDULED): " + departure['arrival']['scheduled'] + "\n"
+                    answer += "Minutes (SCHEDULED): " + str(round(((datetime.strptime(departure['arrival']['scheduled'], "%H:%M:%S") - datetime.strptime(convertUTCToRomeTime(datetime.now()).strftime("%H:%M:%S"), "%H:%M:%S")).total_seconds()) / 60)) + "\n\n"
+
+    return (answer)
+
+def convertUTCToRomeTime(dateTime):
+
+    RomeTimeZone = pytz.timezone('Europe/Rome')
+    
+    dateTime = dateTime.astimezone(RomeTimeZone)
+    
+    return dateTime
