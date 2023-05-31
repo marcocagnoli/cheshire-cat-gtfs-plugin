be14774578fce34b4a31a204edd20b55ea51a382
marco-cagnoli <marco.cagnoli@romamobilita.it>
2023-05-31 08:55:26 +0200

    Table introduced

--- gtfs/gtfs.py
+++ gtfs/gtfs.py
@@ -7,17 +7,22 @@ from os.path import exists
 
 @tool(return_direct=True)
 def bus_stop_eta (stop_code: str, cat):
-    """Use to retrieve what bus is arriving at a stop. Input is the code (or the number) of the bus stop. When you use this tool, do not use any other information from context or memory for the answer to the user question."""
-
+    """Use to retrieve which bus is arriving to a bus stop. Input is the bus stop code."""
+    
     conf = CheckConfiguration()
     
     if conf == None or conf["apikey"] == None:
-        return "Sorry but Transit Land api key is missing in the GTFS plugin configuration file. I need an api key to retrieve what bus is arriving at a stop. Please, visit https://www.transit.land/ to get a free api key and write it back."
+        answer = """
+    In order to give you these information, I need three configuration parameters. I need to save Api Key.
+
+    Which is Tranit Land Api Key?
+    """
+        return answer
     elif conf["onestopid"] == None:
-        return "Sorry but Transit Land OneStopID is missing in the GTFS plugin configuration file. I need it to retrieve what bus is arriving at a stop. Please, visit https://www.transit.land/ to get your OneStopID and write it back."
+        return "I need to save Transit Land OneStopID. Which is Transit Land OneStopID?"
     elif conf["timezone"] == None:
-        return "Sorry but GTFS Timezone is missing in the GTFS plugin configuration file. I need it to retrieve what bus is arriving at a stop. Please, visit https://www.transit.land/ to find the correct TimeZone and write it back."
-    
+        return "I need to save GTFS TimeZone. Which is GTFS TimeZone?"
+
     # ----- start configuration ---------
     
     # get a free api key from https://www.transit.land/
@@ -45,7 +50,12 @@ def bus_stop_eta (stop_code: str, cat):
         
         stop_name = stop["stop_name"]
         
-        answer = "These are next arrivals at bus stop \"" + stop_name + "\" (" + stop_code + "):\n\n"
+        answer = "These are next arrivals at bus stop\n"
+        answer += "&nbsp;\n"
+        answer += "**" + stop_name + "** (" + stop_code + "):\n"
+        answer += "&nbsp;\n"
+        answer += "| &nbsp; Route &nbsp; | &nbsp; &nbsp; &nbsp; Company &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp; Destination &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp; Minutes &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp; Real Time &nbsp; &nbsp; &nbsp; |\n"
+        answer += "|:----------:|:----------:|:----------:|:----------:|:----------:|\n"
     
         routes = []
         
@@ -61,19 +71,12 @@ def bus_stop_eta (stop_code: str, cat):
                 if departure['trip']['route']['route_short_name'] not in routes:
                     routes.append(departure['trip']['route']['route_short_name'])
                 
-                if len(routes) == 1:
-                    answer += "REAL TIME DATA" + "\n\n"
-                
-                answer += "Route: " + departure['trip']['route']['route_short_name'] + "\n"
-                answer += "Company: " + departure['trip']['route']['agency']['agency_name'] +"\n"
-                answer += "Destination: " + departure['trip']['trip_headsign'] + "\n"
-
                 minutes = round((datetime.strptime(departure['service_date'] + " " + departure['arrival']['estimated'], "%Y-%m-%d %H:%M:%S") - convertUTCToGTFSTime(datetime.utcnow(), gtfstimezone)).total_seconds() / 60)
                 
                 if minutes < 0:
                     minutes = 0
                         
-                answer += "Minutes: " + str(minutes) + "\n\n"
+                answer += "|" + departure['trip']['route']['route_short_name'] + "|" + departure['trip']['route']['agency']['agency_name'] + "|"+ departure['trip']['trip_headsign'] + "|" + str(minutes) + "|YES|\n"
                 
         l = len(routes)
         
@@ -89,26 +92,19 @@ def bus_stop_eta (stop_code: str, cat):
                 if departure['trip']['route']['route_short_name'] not in routes:
                     routes.append(departure['trip']['route']['route_short_name'])
                 
-                    if (len(routes) -l) == 1:
-                        answer += "SCHEDULED DATA" + "\n\n"
-                
-                    answer += "Route: " + departure['trip']['route']['route_short_name'] + "\n"
-                    answer += "Company: " + departure['trip']['route']['agency']['agency_name'] +"\n"
-                    answer += "Destination: " + departure['trip']['trip_headsign'] + "\n"
-                    
                     minutes = round((datetime.strptime(departure['service_date'] + " " + departure['arrival']['scheduled'], "%Y-%m-%d %H:%M:%S") - convertUTCToGTFSTime(datetime.utcnow(), gtfstimezone)).total_seconds() / 60)
                     
                     if minutes < 0:
                         minutes = 0
                         
-                    answer += "Minutes (SCHEDULED): " + str(minutes) + "\n\n"
+                    answer += "|" + departure['trip']['route']['route_short_name'] + "|" + departure['trip']['route']['agency']['agency_name'] + "|"+ departure['trip']['trip_headsign'] + "|" + str(minutes) + "|NO|\n"
 
     return (answer)
 
 @tool(return_direct=True)
 def save_apikey (apikey: str, cat):
-    """Use to save Transit Land api key needed by bus_stop_eta function to retrieve transit data. Input is the api key."""
-
+    """Use this tool to save Transit Land Api Key. Input is the Api Key."""
+    
     data = ""
     
     if not exists('./cat/plugins/gtfs/gtfs-config.json'):
@@ -121,16 +117,28 @@ def save_apikey (apikey: str, cat):
     
     else:
         
-        data.apikey = apikey
+        with open('./cat/plugins/gtfs/gtfs-config.json') as json_file:
+            data = json.load(json_file)
         
+            data["apikey"] = apikey
+            
     with open('./cat/plugins/gtfs/gtfs-config.json', 'w') as outfile:
         json.dump(data, outfile)
-        
-    return "Ok, I've saved the api key. At what bus stop are you waiting?"
+    
+    conf = CheckConfiguration()
+    
+    if conf == None or conf["apikey"] == None:
+        return "I need to save Api Key. Which is Tranit Land Api Key?"
+    elif conf["onestopid"] == None:
+        return "I need to save Transit Land OneStopID. Which is Transit Land OneStopID?"
+    elif conf["timezone"] == None:
+        return "I need to save GTFS TimeZone. Which is GTFS TimeZone?"
+    else:       
+        return "Ok, I've saved the api key. At what bus stop are you waiting?"
 
 @tool(return_direct=True)
 def save_onestopid (onestopid: str, cat):
-    """Use to save Transit Land onestopid feed needed by bus_stop_eta function to retrieve transit data. Input is the api key."""
+    """Use this tool to save Transit Land OneStopID. Input is the OneStopID."""
 
     data = ""
     
@@ -152,11 +160,20 @@ def save_onestopid (onestopid: str, cat):
     with open('./cat/plugins/gtfs/gtfs-config.json', 'w') as outfile:
         json.dump(data, outfile)
         
-    return "Ok, I've saved the OneStopID. At what bus stop are you waiting?"
+    conf = CheckConfiguration()
+    
+    if conf == None or conf["apikey"] == None:
+        return "I need to save Api Key. Which is Tranit Land Api Key?"
+    elif conf["onestopid"] == None:
+        return "I need to save Transit Land OneStopID. Which is Transit Land OneStopID?"
+    elif conf["timezone"] == None:
+        return "I need to save GTFS TimeZone. Which is GTFS TimeZone?"
+    else:       
+        return "Ok, I've saved the api key. At what bus stop are you waiting?"
 
 @tool(return_direct=True)
 def save_timezone (timezone: str, cat):
-    """Use to save GTFS timezone feed needed by bus_stop_eta function to retrieve transit data. Input is the api key."""
+    """Use this tool to save GTFS TimeZone. Input is the GTFS TimeZone."""
 
     data = ""
     
@@ -190,9 +207,6 @@ def convertUTCToGTFSTime(utc, gtfstimezone):
     return datetime.strptime(datetime.strftime(utc.astimezone(to_zone), "%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S")
 
 def CheckConfiguration():
-    
-    # return
-    # 1 - configuration file doesn't exists
     
     if not exists('./cat/plugins/gtfs/gtfs-config.json'):
         return None
--- gtfs/prompt.py
+++ gtfs/prompt.py
@@ -8,14 +8,74 @@ from cat.mad_hatter.decorators import hook
 def agent_prompt_chat_history(chat_history, cat):
     history = ""
     tmp = ""
+    who = ""
+    message = ""
     for turn in chat_history:
 
         if turn["message"][0:35] != "These are next arrivals at bus stop":
             history += tmp
             tmp = f"\n - {turn['who']}: {turn['message']}"
+            message = turn["message"]
+            who = turn["who"]
         else:
             tmp = ""
-            
-    history += tmp
     
+    if history != "" and (' '.join(message.split())  == "In order to give you these information, I need three configuration parameters. I need to save Api Key. Which is Tranit Land Api Key?" or message == "I need to save Transit Land OneStopID. Which is Transit Land OneStopID?" or message == "I need to save GTFS TimeZone. Which is GTFS TimeZone?"):
+        history = f"\n - {who}: {message}"
+    else:    
+        history += tmp
+        
     return history
+
+@hook(priority=1)
+def agent_prompt_episodic_memories(memory_docs, cat):
+    # convert docs to simple text
+    memory_texts = [m[0].page_content.replace("\n", ". ") for m in memory_docs]
+
+    # add time information (e.g. "2 days ago")
+    memory_timestamps = []
+    for m in memory_docs:
+        timestamp = m[0].metadata["when"]
+        delta = timedelta(seconds=(time.time() - timestamp))
+        memory_timestamps.append(f" ({verbal_timedelta(delta)})")
+
+    memory_texts = [a + b for a, b in zip(memory_texts, memory_timestamps)]
+
+    memories_separator = "\n  - "
+    memory_content = memories_separator + memories_separator.join(memory_texts)
+
+    f = open("./cat/plugins/gtfs/memory.txt", "a")
+    f.write(memory_content)
+    f.close()
+    
+    memory_content = "\n"
+    
+    return memory_content
+
+@hook(priority=1)
+def agent_prompt_declarative_memories(memory_docs, cat):
+    # convert docs to simple text
+    memory_texts = [m[0].page_content.replace("\n", ". ") for m in memory_docs]
+
+    # add source information (e.g. "extracted from file.txt")
+    memory_sources = []
+    for m in memory_docs:
+        source = m[0].metadata["source"]
+        memory_sources.append(f" (extracted from {source})")
+
+    memory_texts = [a + b for a, b in zip(memory_texts, memory_sources)]
+
+    memories_separator = "\n  - "
+    memory_content = memories_separator + memories_separator.join(memory_texts)
+
+    memory_content = "\n"
+
+    return memory_content
+
+@hook(priority=1)
+def agent_prompt_suffix(cat):
+    suffix = """Conversation until now:{chat_history}
+ - Human: {input}
+
+{agent_scratchpad}"""
+    return suffix
